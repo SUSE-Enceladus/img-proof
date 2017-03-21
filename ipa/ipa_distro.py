9c30d41024f701d31f92186fd32e906241fcef68
Sean Marlow <sean.marlow@suse.com>
2017-03-21 12:40:58 -0600

    Move sudo command to an instance method.

--- ipa/ipa_distro.py
+++ ipa/ipa_distro.py
@@ -16,13 +16,17 @@ class Distro(object):
     def __init__(self):
         super(Distro, self).__init__()
 
+    def get_reboot_cmd(self):
+        """Return reboot command for given distribution."""
+        return 'shutdown -r now'
+
     def get_stop_ssh_service_cmd(self):
         """Return command to stop SSH service on given distribution."""
         raise NotImplementedError('Implement method in child classes.')
 
-    def get_reboot_cmd(self):
-        """Return reboot command for given distribution."""
-        return 'shutdown -r now'
+    def get_sudo_cmd(self):
+        """Return sudo command to wrap one more commands."""
+        return 'sudo sh -c'
 
     def reboot(self, instance_ip, ssh_private_key, ssh_user, sudo=True):
         """Execute reboot command on instance."""
@@ -31,7 +35,7 @@ class Distro(object):
             self.get_reboot_cmd()
         )
         if sudo:
-            reboot_cmd = "sudo sh -c '%s'" % reboot_cmd
+            reboot_cmd = "{} '{}'".format(self.get_sudo_cmd(), reboot_cmd)
 
         print('Rebooting instance: %s\n' % reboot_cmd)
         out, err = ipa_utils.execute_ssh_command(reboot_cmd,
