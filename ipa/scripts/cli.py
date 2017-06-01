822b2c146a9ee5f48e60d7b8c038514a77884b6d
Sean Marlow <sean.marlow@suse.com>
2017-06-01 13:40:20 -0600

    Start cli implementation of test image endpoint.
    - Rename cli group to main

--- ipa/scripts/cli.py
+++ ipa/scripts/cli.py
@@ -1,3 +1,4 @@
+"""Ipa CLI endpoints using click library."""
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
@@ -7,23 +8,64 @@
 #
 # See LICENSE for license information.
 
-import click
 import sys
+
+import click
+
+from ipa.ipa_constants import SUPPORTED_DISTROS, SUPPORTED_PROVIDERS
+from ipa.scripts.cli_constants import (
+    HELP_DISTRO,
+    HELP_RUNNING_INSTANCE,
+    HELP_SSH_PRIVATE_KEY,
+    HELP_SSH_USER,
+)
 
 
 @click.group()
 @click.version_option()
-def ipa():
+def main():
+    """
+    Ipa provides a Python API and command line utility for testing images.
+    It can be used to test images in the Public Cloud (AWS, Azure, GCE, etc.).
+    """
     pass
 
 
 @click.command()
-def test():
+@click.argument('provider',
+                type=click.Choice(SUPPORTED_PROVIDERS))
+@click.option('-d',
+              '--distro',
+              type=click.Choice(SUPPORTED_DISTROS),
+              help=HELP_DISTRO)
+@click.option('-R', '--running-instance', help=HELP_RUNNING_INSTANCE)
+@click.option('-p',
+              '--ssh-private-key',
+              type=click.Path(exists=True),
+              help=HELP_SSH_PRIVATE_KEY)
+@click.option('-u', '--ssh-user', help=HELP_SSH_USER)
+def test(provider,
+         distro,
+         running_instance,
+         ssh_private_key,
+         ssh_user):
     """Test image in the given cloud framework using the supplied test file."""
+    click.secho('Testing image...', fg='green')
     try:
-        raise Exception('Test command not implemented :( ... yet.')
+        status, results = test_image(
+            provider,
+            distro,
+            running_instance,
+            ssh_private_key,
+            ssh_user,
+        )
+        # TODO: print results
+        sys.exit(status)
     except Exception as e:
-        click.echo(click.style("Broken: %s" % e, fg='red'))
+        click.secho(
+            "{}: {}".format(type(e).__name__, e),
+            fg='red'
+        )
         sys.exit(1)
 
 
@@ -50,5 +92,5 @@ ipa.add_command(results)
 ipa.add_command(list_tests)
 
 if __name__ == "__main__":
-    ipa()
+    main()
 
