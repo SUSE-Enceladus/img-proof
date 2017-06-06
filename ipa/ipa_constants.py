6314c222315f83c3382b4bd59c7bd0e7d37d0bdd
Sean Marlow <sean.marlow@suse.com>
2017-06-06 13:41:58 -0600

    Update test discovery functions.
    - Update test discovery to account for any combo of sync points.
    - Cleanup docstrings.
    - Re-order functions in alpha order.

--- ipa/ipa_constants.py
+++ ipa/ipa_constants.py
@@ -1,3 +1,10 @@
+"""ipa constants."""
+
 NOT_IMPLEMENTED = 'Implement method in child classes.'
 SUPPORTED_DISTROS = ('openSUSE_Leap', 'SLES')
 SUPPORTED_PROVIDERS = ('Azure', 'EC2', 'GCE')
+
+SYNC_POINTS = (
+    'test_hard_reboot',
+    'test_soft_reboot'
+)
--- ipa/ipa_utils.py
+++ ipa/ipa_utils.py
@@ -1,3 +1,4 @@
+"""Utility methods."""
 # -*- coding: utf-8 -*-
 #
 # Copyright (c) 2017 SUSE LLC
@@ -20,6 +21,7 @@ from contextlib import contextmanager
 from string import ascii_lowercase
 from tempfile import NamedTemporaryFile
 
+from ipa.ipa_constants import SYNC_POINTS
 from ipa.ipa_exceptions import IpaSSHException, IpaUtilsException
 
 import paramiko
@@ -77,7 +79,15 @@ def establish_ssh_connection(ip,
 
 
 def execute_ssh_command(client, cmd):
-    """Execute given command using paramiko and return stdout, stderr."""
+    """
+    Execute given command using paramiko.
+
+    Returns:
+        String output of cmd execution.
+    Raises:
+        IpaSSHException: If stderr returns a non-empty string.
+
+    """
     try:
         stdin, stdout, stderr = client.exec_command(cmd)
         err = stderr.read()
@@ -89,92 +99,65 @@ def execute_ssh_command(client, cmd):
     return out
 
 
-def find_test_files(test_dirs, names=None):
-    """Walk all dirs and find path of given test file names.
-
-    If names is None find all test files in the given test
-    directories and return the list.
-
-    Raise IpaUtilsException:
-    - If there are multiple test files or test descriptions with the
-      same name.
-    - If a test file or test description cannot be found.
-    - If there is a name overlap with a test file and test
-      description.
+def expand_test_files(test_dirs, names):
     """
-    if names and not isinstance(names, list):
+    Expand the list of test files and test descriptions.
+
+    The list is split on sync points and duplicates in
+    each set are removed.
+
+    Returns:
+        List of test files split into sets by sync points.
+    Raises:
+        IpaUtilsException: If names is not a list.
+
+    """
+    if not isinstance(names, list):
         raise IpaUtilsException(
             'Names must be a list containing test names'
             ' and/or test descriptions.'
         )
 
-    tests = {}
-    descriptions = {}
-    for test_dir in test_dirs:
-        for root, dirs, files in os.walk(test_dir):
-            test_files = fnmatch.filter(files, 'test_*.py')
-            description_files = fnmatch.filter(files, 'test_*.yaml')
+    tests, descriptions = get_test_files(test_dirs)
 
-            for test_file in test_files:
-                path = os.path.join(root, test_file)
-                name, ext = test_file.split('.')
-                if name not in tests:
-                    tests[name] = path
-                else:
-                    raise IpaUtilsException(
-                        'Duplicate test file name found: %s, %s'
-                        % (path, tests.get(name))
-                    )
-
-            for description_file in description_files:
-                path = os.path.join(root, description_file)
-                name, ext = description_file.split('.')
-                if name in tests:
-                    raise IpaUtilsException(
-                        'Test description name matches test file: %s, %s'
-                        % (path, tests.get(name))
-                    )
-                elif name not in descriptions:
-                    descriptions[name] = path
-                else:
-                    raise IpaUtilsException(
-                        'Duplicate test description file name found: %s, %s'
-                        % (path, descriptions.get(name))
-                    )
-
-    if not names:
-        tests.update(descriptions)
-        return tests
-
-    names = set(names)
-    description_names = names & set(descriptions)
-    names = names - description_names
-    for name in description_names:
-        names.update(
-            get_tests_from_description(
-                    name,
-                    descriptions
+    expanded_names = []
+    for name in names:
+        if name in descriptions:
+            expanded_names += get_tests_from_description(
+                name,
+                descriptions
             )
+        else:
+            expanded_names.append(name)
+
+    return parse_sync_points(expanded_names, tests)
+
+
+def find_test_file(name, tests):
+    """
+    Find test file by name, given a list of tests.
+
+    If a specific test case is appended to test
+    name, split the case and append to path.
+
+    Raises:
+        IpaUtilsException: If test file not found.
+
+    """
+    try:
+        test_name, test_case = name.split('::', 1)
+    except:
+        test_name, test_case = name, None
+
+    path = tests.get(test_name, None)
+    if not path:
+        raise IpaUtilsException(
+            'Test file with name: %s cannot be found.' % test_name
         )
 
-    test_files = {}
-    for name in names:
-        try:
-            test_name, test_case = name.split('::', 1)
-        except:
-            test_name, test_case = name, None
-
-        path = tests.get(test_name, None)
-        if not path:
-            raise IpaUtilsException(
-                'Test file with name: %s cannot be found.' % test_name
-            )
-
-        if test_case:
-            path = ''.join([path, '::', test_case])
-        test_files[name] = path
-
-    return test_files
+    if test_case:
+        path = ''.join([path, '::', test_case])
+    return path
 
 
 def get_config(config_path):
@@ -198,7 +181,7 @@ def get_config(config_path):
 
 
 def get_from_config(config, section, default_section, entry):
-    """Retrieve an entry from the configuration"""
+    """Retrieve an entry from the configuration."""
     value = None
     with ignored(ConfigParser.Error):
         value = config.get(section, entry)
@@ -211,50 +194,6 @@ def get_from_config(config, section, default_section, entry):
                 'Unable to get %s value from config' % entry
             )
     return value
-
-
-def get_tests_from_description(name,
-                               descriptions,
-                               parsed=None):
-    """Recursively collect all tests in test description.
-    Args:
-        name (str): Yaml test description file name.
-        descriptions (dict): Dict of test description name
-                             (key) and absolute file paths
-                             (value).
-        parsed (list): List of description paths which have
-                       already been parsed to prevent infinte
-                       recursion.
-    """
-    tests = []
-    if not parsed:
-        parsed = []
-
-    description = descriptions.get(name, None)
-    if not description:
-        raise IpaUtilsException(
-            'Test description file with name: %s cannot be located.'
-            % name
-        )
-
-    if description in parsed:
-        return tests
-
-    parsed.append(description)
-    test_data = get_yaml_config(description)
-
-    if 'tests' in test_data:
-        tests += test_data.get('tests')
-
-    if 'include' in test_data:
-        for description_name in test_data.get('include'):
-            tests += get_tests_from_description(
-                description_name,
-                descriptions,
-                parsed
-            )
-
-    return tests
 
 
 def get_random_string(length=12):
@@ -297,10 +236,111 @@ def get_ssh_client(ip,
     )
 
 
-def get_yaml_config(config_path):
-    """Load yaml config file and return dictionary.
+def get_test_files(test_dirs):
+    """
+    Walk all test dirs and find all tests and test descriptions.
 
-    TODO: This will need refactoring similar to the test search.
+    Returns:
+        A tuple containing a dict mapping test names to full path
+        and a dict mapping test descriptions to full path.
+    Raises:
+        IpaUtilsException: If there are multiple test files or
+            test descriptions with the same name. Or, if there is a
+            name overlap with a test file and test description.
+
+    """
+    tests = {}
+    descriptions = {}
+    for test_dir in test_dirs:
+        for root, dirs, files in os.walk(test_dir):
+            test_files = fnmatch.filter(files, 'test_*.py')
+            description_files = fnmatch.filter(files, 'test_*.yaml')
+
+            for test_file in test_files:
+                path = os.path.join(root, test_file)
+                name, ext = test_file.split('.')
+                if name not in tests:
+                    tests[name] = path
+                else:
+                    raise IpaUtilsException(
+                        'Duplicate test file name found: %s, %s'
+                        % (path, tests.get(name))
+                    )
+
+            for description_file in description_files:
+                path = os.path.join(root, description_file)
+                name, ext = description_file.split('.')
+                if name in tests:
+                    raise IpaUtilsException(
+                        'Test description name matches test file: %s, %s'
+                        % (path, tests.get(name))
+                    )
+                elif name not in descriptions:
+                    descriptions[name] = path
+                else:
+                    raise IpaUtilsException(
+                        'Duplicate test description file name found: %s, %s'
+                        % (path, descriptions.get(name))
+                    )
+
+    return tests, descriptions
+
+
+def get_tests_from_description(name,
+                               descriptions,
+                               parsed=None):
+    """
+    Recursively collect all tests in test description.
+
+    Args:
+        name (str): Yaml test description file name.
+        descriptions (dict): Dict of test description name
+                             (key) and absolute file paths
+                             (value).
+        parsed (list): List of description paths which have
+                       already been parsed to prevent infinte
+                       recursion.
+    Returns:
+        A list of expanded test files.
+
+    """
+    tests = []
+    if not parsed:
+        parsed = []
+
+    description = descriptions.get(name, None)
+    if not description:
+        raise IpaUtilsException(
+            'Test description file with name: %s cannot be located.'
+            % name
+        )
+
+    if description in parsed:
+        return tests
+
+    parsed.append(description)
+    test_data = get_yaml_config(description)
+
+    if 'tests' in test_data:
+        tests += test_data.get('tests')
+
+    if 'include' in test_data:
+        for description_name in test_data.get('include'):
+            tests += get_tests_from_description(
+                description_name,
+                descriptions,
+                parsed
+            )
+
+    return tests
+
+
+def get_yaml_config(config_path):
+    """
+    Load yaml config file and return dictionary.
+
+    Todo:
+        * This will need refactoring similar to the test search.
     """
     config_path = os.path.expanduser(config_path)
     if not os.path.isfile(config_path):
@@ -320,6 +360,34 @@ def ignored(*exceptions):
         yield
     except exceptions:
         pass
+
+
+def parse_sync_points(names, tests):
+    """
+    Slice list of test names on sync points.
+
+    If test is test file find full path to file.
+
+    Returns:
+        A list of test file sets and sync point strings.
+        e.g. [set('test1', 'test2'), 'test_soft_reboot']
+
+    """
+    test_files = []
+    section = set()
+
+    for name in names:
+        if name in SYNC_POINTS:
+            if section:
+                test_files.append(section)
+                section = set()
+            test_files.append(name)
+        else:
+            section.add(find_test_file(name, tests))
+
+    if section:
+        test_files.append(section)
+    return test_files
 
 
 @contextmanager
