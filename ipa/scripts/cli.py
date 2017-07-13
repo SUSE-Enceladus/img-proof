c344fb462deed92732004f778be323a09f711538
Sean Marlow <sean.marlow@suse.com>
2017-07-13 10:03:13 -0600

    Change license to GPLv3.

--- ipa/scripts/cli.py
+++ ipa/scripts/cli.py
@@ -5,9 +5,21 @@
 
 # Copyright (c) 2017 SUSE LLC
 #
-# This file is part of ipa.
+# This file is part of ipa. Ipa provides an api and command line
+# utilities for testing images in the Public Cloud.
 #
-# See LICENSE for license information.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
 import sys
@@ -17,9 +29,29 @@ import click
 from ipa.ipa_constants import SUPPORTED_DISTROS, SUPPORTED_PROVIDERS
 from ipa.ipa_controller import test_image
 
+LICENSE = '''ipa Copyright (C) 2017 SUSE LLC
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it
+under certain conditions; See LICENSE for details.'''
+
+
+def print_license(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo(LICENSE)
+    ctx.exit()
+
 
 @click.group()
 @click.version_option()
+@click.option(
+    '--license',
+    is_flag=True,
+    callback=print_license,
+    expose_value=False,
+    is_eager=True,
+    help='Show license information and exit.'
+)
 def main():
     """
     Ipa provides a Python API and command line utility for testing images.
