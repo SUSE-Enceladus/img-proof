20217804f6faa7046cc7e3e6ac0afae662127359
Sean Marlow <sean.marlow@suse.com>
2017-08-15 08:52:53 -0600

    Use /usr/bin/python3 shebang.

--- tests/azure/test_azure.py
+++ tests/azure/test_azure.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 """Integration tests for Azure provider."""
--- tests/ec2/test_ec2.py
+++ tests/ec2/test_ec2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 """Integration tests for EC2 provider."""
