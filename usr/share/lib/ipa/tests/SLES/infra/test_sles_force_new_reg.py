4e9045247e95aba69033db7d520a8a407c88800b
Sean Marlow <sean.marlow@suse.com>
2018-05-25 09:31:50 -0600

    Fix typo in force new reg test.

--- usr/share/lib/ipa/tests/SLES/infra/test_sles_force_new_reg.py
+++ usr/share/lib/ipa/tests/SLES/infra/test_sles_force_new_reg.py
@@ -1,5 +1,5 @@
 def test_sles_force_new_reg(check_cloud_register, host):
     host.run("sudo sh -c ': > /var/log/cloudregister'")
-    result = host.run('sudo registsercloudguest --force-new')
+    result = host.run('sudo registercloudguest --force-new')
     assert result.rc == 0
     assert check_cloud_register()
