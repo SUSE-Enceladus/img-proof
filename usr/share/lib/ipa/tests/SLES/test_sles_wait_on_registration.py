39f38d08a60b8d1a02aef2fd49f5bcb30035ef29
Sean Marlow <sean.marlow@suse.com>
2018-05-31 15:07:46 -0600

    Add a test to wait on registration for on-demand.
    On-demand registration will take time to complete. Wait for the
    process to terminate before moving forward with tests.

---
+++ usr/share/lib/ipa/tests/SLES/test_sles_wait_on_registration.py
@@ -0,0 +1,18 @@
+import time
+
+
+def test_sles_wait_on_registration(host):
+    start = time.time()
+    end = start + 600
+
+    while time.time() < end:
+        result = host.run('pgrep registercloud')
+
+        if not result.stdout.strip():
+            break
+        else:
+            time.sleep(10)
+    else:
+        raise Exception(
+            'Registration did not finish properly for on-demand instance.'
+        )
