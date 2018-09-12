c13ec51ca52dfb0c272bc56188bc22fe0b551d1f
Sean Marlow <sean.marlow@suse.com>
2018-09-12 15:18:02 -0600

    Bump version: 2.0.0 → 2.0.1

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ tox_requirements = [
 
 setup(
     name='python3-ipa',
-    version='2.0.0',
+    version='2.0.1',
     description="Package for automated testing of cloud images.",
     long_description=readme,
     long_description_content_type="text/markdown",
