07d54c4e291f17242d1bf9a736741ce13e66b922
Sean Marlow <sean.marlow@suse.com>
2018-11-09 15:49:37 -0700

    Update version in docs config.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'SUSE'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '2.1.0'
+release = '2.2.0'
 
 
 # -- General configuration ---------------------------------------------------
