6213f2ec3b32d0c8c20dd9c3c70eb5d8bf13bcd2
Clemens Famulla-Conrad <cfamullaconrad@suse.de>
2018-12-19 11:30:19 +0100

    treat uuid always lowercase
    Since [1] the uuid is created with lowercase letters.
    
    [1] https://github.com/torvalds/linux/commit/712ff25450bd01366301eef81c33e865d901e7b7
    
    Signed-off-by: Clemens Famulla-Conrad <cfamullaconrad@suse.de>

--- usr/share/lib/ipa/tests/SLES/EC2/test_sles_ec2_uuid.py
+++ usr/share/lib/ipa/tests/SLES/EC2/test_sles_ec2_uuid.py
@@ -2,4 +2,4 @@
 
 def test_sles_ec2_uuid(host):
     result = host.run('sudo cat /sys/devices/virtual/dmi/id/product_uuid')
-    assert result.stdout.startswith('EC2')
+    assert result.stdout[:3].lower() == 'ec2'
