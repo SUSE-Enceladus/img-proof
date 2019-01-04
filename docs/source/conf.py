01b58e7360bd4dfc7201c14629a0a9335eb02f97
Sean Marlow <sean.marlow@suse.com>
2019-01-04 10:13:39 -0600

    Bump version: 2.5.0 → 2.6.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'SUSE'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '2.5.0'
+release = '2.6.0'
 
 
 # -- General configuration ---------------------------------------------------
