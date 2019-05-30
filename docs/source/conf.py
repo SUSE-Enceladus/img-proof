cb2a5447d805e0c756059fdea9b0c7223c02c25f
Sean Marlow <sean.marlow@suse.com>
2019-05-30 15:50:39 -0500

    Bump version: 3.3.1 → 4.0.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'SUSE'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '3.3.1'
+release = '4.0.0'
 
 
 # -- General configuration ---------------------------------------------------
