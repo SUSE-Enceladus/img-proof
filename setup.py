0cbcf96d7d772d70b45426eaa6dd8542b952d224
Sean Marlow <sean.marlow@suse.com>
2020-03-10 16:29:06 -0500

    Bump version: 4.8.0 → 4.8.1

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ tox_requirements = [
 
 setup(
     name='img-proof',
-    version='4.8.0',
+    version='4.8.1',
     description="Package for automated testing of cloud images.",
     long_description=readme,
     long_description_content_type="text/markdown",
