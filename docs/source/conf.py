5de8540d7bb24811685db7279461c307eeebb9ae
Sean Marlow <sean.marlow@suse.com>
2020-08-18 14:33:27 -0500

    Bump version: 5.3.0 → 6.0.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'SUSE'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '5.3.0'
+release = '6.0.0'
 
 
 # -- General configuration ---------------------------------------------------
