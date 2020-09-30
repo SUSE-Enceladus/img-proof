de217a02eb53749995e16e2e63763ba6c9c14617
Sean Marlow <sean.marlow@suse.com>
2020-09-30 11:21:39 -0500

    Bump version: 6.0.0 → 6.1.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'SUSE'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '6.0.0'
+release = '6.1.0'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ tox_requirements = [
 
 setup(
     name='img-proof',
-    version='6.0.0',
+    version='6.1.0',
     description="Package for automated testing of cloud images.",
     long_description=readme,
     long_description_content_type="text/markdown",
