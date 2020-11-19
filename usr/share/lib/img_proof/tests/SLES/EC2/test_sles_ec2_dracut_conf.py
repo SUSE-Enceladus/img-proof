4dbb258490d3949591241a45e9925868b4761ca1
Sean Marlow <sean.marlow@suse.com>
2020-11-19 15:59:25 -0600

    Fix typos in dracut test.
    Cleanup test, explicit check if conf file exists.

--- usr/share/lib/img_proof/tests/SLES/EC2/test_sles_ec2_dracut_conf.py
+++ usr/share/lib/img_proof/tests/SLES/EC2/test_sles_ec2_dracut_conf.py
@@ -1,6 +1,18 @@
-def test_sles_dracut_conf(host):
-    needed_drivers = ('ena', 'nvme', 'nvme-core', 'virtio', 'virtio_scsi',
-                      'xen-blkfront', 'xen-netfront')
+def test_sles_ec2_dracut_conf(host):
+    needed_drivers = (
+        'ena',
+        'nvme',
+        'nvme-core',
+        'virtio',
+        'virtio_scsi',
+        'xen-blkfront',
+        'xen-netfront'
+    )
+
     dracut_conf = host.file('/etc/dracut.conf.d/07-aws-type-switch.conf')
+
+    assert dracut_conf.exists
+    assert dracut_conf.is_file
+
     for driver in needed_drivers:
         assert dracut_conf.contains(driver)
