a49c47b04e8df8e686d499a9a70a98948967b7fc
Sean Marlow <sean.marlow@suse.com>
2020-12-04 09:42:37 -0600

    Split kernel command line args before compare.
    This removes descrepencies in white space due to placement of
    multipath arg.

--- usr/share/lib/img_proof/tests/SLES/test_sles_multipath_off.py
+++ usr/share/lib/img_proof/tests/SLES/test_sles_multipath_off.py
@@ -1,3 +1,4 @@
 def test_sles_multipath_off(host):
-    kernel_cmdline = host.file('/proc/cmdline')
-    assert kernel_cmdline.contains(' multipath=off ')
+    kernel_cmdline = host.run('cat /proc/cmdline')
+    args = kernel_cmdline.stdout.split()
+    assert 'multipath=off' in args
