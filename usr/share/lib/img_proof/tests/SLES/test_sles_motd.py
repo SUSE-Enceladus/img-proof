1640a17d6bf799e740ab23676a296e1406551f4f
Sean Marlow <sean.marlow@suse.com>
2021-05-10 16:11:20 -0500

    Check if motd file exists and is a file
    Before checking the file contents. To make an error state more
    clear.

--- usr/share/lib/img_proof/tests/SLES/test_sles_motd.py
+++ usr/share/lib/img_proof/tests/SLES/test_sles_motd.py
@@ -2,6 +2,8 @@ def test_sles_motd(host, get_release_value):
     motd = host.file('/etc/motd')
     version = get_release_value('VERSION')
     assert version
+    assert motd.exists
+    assert motd.is_file
     assert motd.contains(
         'SUSE Linux Enterprise Server {0}'.format(
             version.replace('-', ' ')
