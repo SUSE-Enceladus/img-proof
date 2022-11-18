dcc4fb6d95dbab051cc8d2465818ace91d36aec0
Sean Marlow <sean.marlow@suse.com>
2022-11-18 14:36:27 -0500

    Run dmesg as root
    This is required for hardened images

--- usr/share/lib/img_proof/tests/SLES/GCE/test_sles_gce_sev.py
+++ usr/share/lib/img_proof/tests/SLES/GCE/test_sles_gce_sev.py
@@ -1,4 +1,7 @@
 def test_sles_gce_sev(host):
     expected = 'SEV'
-    output = host.run('dmesg')
+
+    with host.sudo():
+        output = host.run('dmesg')
+
     assert expected in output.stdout.strip()
