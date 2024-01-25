a134371f1ddbaa1dee946bfa50360e44905f5592
Sean Marlow <sean.marlow@suse.com>
2024-01-25 13:47:51 -0500

    Bump version: 7.22.0 → 7.23.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'SUSE'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '7.22.0'
+release = '7.23.0'
 
 
 # -- General configuration ---------------------------------------------------
--- img_proof/__init__.py
+++ img_proof/__init__.py
@@ -22,4 +22,4 @@
 
 __author__ = """SUSE"""
 __email__ = 'public-cloud-dev@susecloud.net'
-__version__ = '7.22.0'
+__version__ = '7.23.0'
