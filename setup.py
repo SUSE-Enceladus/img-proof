d11974e2ad48df160371a32f53ba9235655f4764
Sean Marlow <sean.marlow@suse.com>
2024-02-22 16:47:17 -0500

    Bump version: 7.23.0 → 7.24.0

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ tox_requirements = [
 
 setup(
     name='img-proof',
-    version='7.23.0',
+    version='7.24.0',
     description="Package for automated testing of cloud images.",
     long_description=readme,
     long_description_content_type="text/markdown",
