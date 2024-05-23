0d90be77863306a827728f7da28ffe8fe39b28f1
Sean Marlow <sean.marlow@suse.com>
2024-05-23 16:55:10 -0400

    Bump version: 7.29.1 → 7.29.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'SUSE'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '7.29.1'
+release = '7.29.2'
 
 
 # -- General configuration ---------------------------------------------------
