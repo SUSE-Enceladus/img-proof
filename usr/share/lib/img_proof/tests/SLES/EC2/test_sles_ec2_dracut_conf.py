4849014940aaf35987ac993cb3e9ff21b7814dda
Andres Pozo <andres.munoz@suse.com>
2025-01-08 14:41:28 +0100

    Includes the EC2 dracut conf  test adapted to the latest changes

--- usr/share/lib/img_proof/tests/SLES/EC2/test_sles_ec2_dracut_conf.py
+++ usr/share/lib/img_proof/tests/SLES/EC2/test_sles_ec2_dracut_conf.py
@@ -5,34 +5,43 @@ def test_sles_ec2_dracut_conf(host, get_release_value, determine_architecture):
     if determine_architecture() != 'X86_64':
         pytest.skip('Only x86_64 architecture is tested.')
 
-    needed_drivers = (
-        'ena',
-        'nvme',
-        'nvme-core',
-        'virtio',
-        'virtio_scsi',
-        'xen-blkfront',
-        'xen-netfront'
-    )
+    needed_drivers = {
+        '07-aws-type-switch.conf': [
+            'ena',
+            'virtio',
+            'virtio_scsi',
+            'xen-blkfront',
+            'xen-netfront'
+        ],
+        '07-nvme.conf': [
+            'nvme',
+            'nvme-core'
+        ]
+    }
+
     version = get_release_value('VERSION')
     assert version
 
     name = get_release_value('PRETTY_NAME').lower()
     assert name
 
-    dracut_conf = host.file('/etc/dracut.conf.d/07-aws-type-switch.conf')
+    base_dracut_config_dir = '/etc/dracut.conf.d/'
 
-    assert dracut_conf.exists
-    assert dracut_conf.is_file
+    for cfg_filename, expected_drivers in needed_drivers.items():
 
-    for driver in needed_drivers:
-        if (
-            driver.startswith('virtio') and
-            (
-                version.startswith('15') or
-                version == '12-SP5' or
-                'micro' in name
-            )
-        ):
-            continue
-        assert dracut_conf.contains(driver)
+        dracut_conf = host.file(base_dracut_config_dir + cfg_filename)
+
+        assert dracut_conf.exists
+        assert dracut_conf.is_file
+
+        for driver in expected_drivers:
+            if (
+                driver.startswith('virtio') and
+                (
+                    version.startswith('15') or
+                    version == '12-SP5' or
+                    'micro' in name
+                )
+            ):
+                continue
+            assert dracut_conf.contains(driver)
