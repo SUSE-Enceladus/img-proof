b4770f808da56e7e68cab02fb4db5ae071df75ca
Andres Pozo <andres.munoz@suse.com>
2025-01-20 15:21:11 +0100

    Implements a valid test for 2 different config dirs

--- usr/share/lib/img_proof/tests/SLES/EC2/test_sles_ec2_dracut_conf.py
+++ usr/share/lib/img_proof/tests/SLES/EC2/test_sles_ec2_dracut_conf.py
@@ -16,9 +16,20 @@ def test_sles_ec2_dracut_conf(host, get_release_value, determine_architecture):
         'nvme-core'
     ]
 
-    base_dracut_config_dir = '/usr/lib/dracut/dracut.conf.d/'
-    config_dir_ls = host.run(f'ls {base_dracut_config_dir}').stdout.strip()
-    config_files = shlex.split(config_dir_ls)
+    base_dracut_config_dirs = [
+        '/usr/lib/dracut/dracut.conf.d/',
+        '/etc/dracut.conf.d/'
+    ]
+    config_files = []
+    for config_dir in base_dracut_config_dirs:
+        cfg_dir = host.file(config_dir)
+        if not cfg_dir.exists or not cfg_dir.is_directory:
+            continue
+
+        config_dir_ls = host.run(f'ls {config_dir}').stdout.strip()
+        config_files_in_dir = shlex.split(config_dir_ls)
+        for config_file in config_files_in_dir:
+            config_files.append(config_dir + config_file)
 
     version = get_release_value('VERSION')
     assert version
@@ -39,7 +50,7 @@ def test_sles_ec2_dracut_conf(host, get_release_value, determine_architecture):
 
         found = False
         for cfg_filename in config_files:
-            dracut_conf = host.file(base_dracut_config_dir + cfg_filename)
+            dracut_conf = host.file(cfg_filename)
             if (
                 dracut_conf.exists and
                 dracut_conf.is_file and
