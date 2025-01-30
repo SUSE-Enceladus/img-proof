7dfec10649a47963c6a33723461dbbf47649255c
Andres Pozo <andres.munoz@suse.com>
2025-01-30 15:01:30 +0100

    Includes the EC2 dracut conf  test adapted to the latest changes (#405)
    * Includes the EC2 dracut conf  test adapted to the latest changes
    
    * Modifies dracut config check to scan all the files in the 2 config dirs

--- usr/share/lib/img_proof/tests/SLES/EC2/test_sles_ec2_dracut_conf.py
+++ usr/share/lib/img_proof/tests/SLES/EC2/test_sles_ec2_dracut_conf.py
@@ -1,29 +1,41 @@
 import pytest
+import shlex
 
 
 def test_sles_ec2_dracut_conf(host, get_release_value, determine_architecture):
     if determine_architecture() != 'X86_64':
         pytest.skip('Only x86_64 architecture is tested.')
 
-    needed_drivers = (
+    needed_drivers = [
         'ena',
-        'nvme',
-        'nvme-core',
         'virtio',
         'virtio_scsi',
         'xen-blkfront',
-        'xen-netfront'
-    )
+        'xen-netfront',
+        'nvme',
+        'nvme-core'
+    ]
+
+    base_dracut_config_dirs = [
+        '/usr/lib/dracut/dracut.conf.d/',
+        '/etc/dracut.conf.d/'
+    ]
+    config_files = []
+    for config_dir in base_dracut_config_dirs:
+        cfg_dir = host.file(config_dir)
+        if not cfg_dir.exists or not cfg_dir.is_directory:
+            continue
+
+        config_dir_ls = host.run(f'ls {config_dir}').stdout.strip()
+        config_files_in_dir = shlex.split(config_dir_ls)
+        for config_file in config_files_in_dir:
+            config_files.append(config_dir + config_file)
+
     version = get_release_value('VERSION')
     assert version
 
     name = get_release_value('PRETTY_NAME').lower()
     assert name
-
-    dracut_conf = host.file('/etc/dracut.conf.d/07-aws-type-switch.conf')
-
-    assert dracut_conf.exists
-    assert dracut_conf.is_file
 
     for driver in needed_drivers:
         if (
@@ -35,4 +47,15 @@ def test_sles_ec2_dracut_conf(host, get_release_value, determine_architecture):
             )
         ):
             continue
-        assert dracut_conf.contains(driver)
+
+        found = False
+        for cfg_filename in config_files:
+            dracut_conf = host.file(cfg_filename)
+            if (
+                dracut_conf.exists and
+                dracut_conf.is_file and
+                dracut_conf.contains(driver)
+            ):
+                found = True
+                break
+        assert found
