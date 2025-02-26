85dae46e8ede1e358c906f15148487e074c647dc
Sean Marlow <sean.marlow@suse.com>
2025-02-26 09:27:27 -0500

    Bump version: 7.35.0 → 7.36.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'SUSE'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '7.35.0'
+release = '7.36.0'
 
 
 # -- General configuration ---------------------------------------------------
--- package/python-img-proof.spec
+++ package/python-img-proof.spec
@@ -27,7 +27,7 @@
 %endif
 
 Name:           python-img-proof
-Version:        7.35.0
+Version:        7.36.0
 Release:        0
 Summary:        Command line and API for testing custom images
 License:        GPL-3.0-or-later
