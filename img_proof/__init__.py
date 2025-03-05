4b0cbf8104a4194c93384a17d3f14a10e2e33038
Sean Marlow <sean.marlow@suse.com>
2025-03-05 14:18:00 -0500

    Bump version: 7.36.0 → 7.36.1

--- img_proof/__init__.py
+++ img_proof/__init__.py
@@ -22,4 +22,4 @@
 
 __author__ = """SUSE"""
 __email__ = 'public-cloud-dev@susecloud.net'
-__version__ = '7.36.0'
+__version__ = '7.36.1'
--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ tox_requirements = [
 
 setup(
     name='img-proof',
-    version='7.36.0',
+    version='7.36.1',
     description="Package for automated testing of cloud images.",
     long_description=readme,
     long_description_content_type="text/markdown",
