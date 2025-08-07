b6f004b9f42160d43c06de6233cd3a7d7db5a2b8
Sean Marlow <sean.marlow@suse.com>
2025-08-07 09:19:51 -0400

    Bump version: 9.2.0 → 9.3.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'SUSE'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '9.2.0'
+release = '9.3.0'
 
 
 # -- General configuration ---------------------------------------------------
--- img_proof/__init__.py
+++ img_proof/__init__.py
@@ -22,4 +22,4 @@
 
 __author__ = """SUSE"""
 __email__ = 'public-cloud-dev@susecloud.net'
-__version__ = '9.2.0'
+__version__ = '9.3.0'
