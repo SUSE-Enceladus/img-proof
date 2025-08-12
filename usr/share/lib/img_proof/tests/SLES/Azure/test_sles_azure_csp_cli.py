a8032cdfa5c2de27a3bdfa4acf37acaf870a0bbd
Sean Marlow <sean.marlow@suse.com>
2025-08-12 12:57:32 -0400

    Handle SLES version parsing in beta stage
    - Add fixtures to check for skipinbeta flag and parse version
    - Cleanup flake8 formatting errors
    - Update SDK tests to use new fixtures

--- usr/share/lib/img_proof/tests/SLES/Azure/test_sles_azure_csp_cli.py
+++ usr/share/lib/img_proof/tests/SLES/Azure/test_sles_azure_csp_cli.py
@@ -1,26 +1,20 @@
 import pytest
-import re
 
-def test_sles_azure_csp_cli(host, get_release_value, is_sle_micro):
-    version = get_release_value('VERSION')
-    assert version
+
+def test_sles_azure_csp_cli(host, is_sle_micro, get_version):
+    version = get_version()
 
     if is_sle_micro():
         pytest.skip('Micro has product version instead of SLE version.')
 
-    match = re.match(r'^(\d+)(?:[.-](?:SP)?(\d+))?$', version)
-    assert match, f"Unexpected version format: {version}"
-
-    major = match.group(1)
-    patchlevel = match.group(2)
-
-    if int(major) < 16:
+    if version < 16.0:
         pytest.skip('Pre-SLE 16.0 versions do not have CSP CLI.')
-
 
     az_cmd = host.run('az --version')
     assert az_cmd.rc == 0, f"'az --version' failed with code {az_cmd.rc}"
-    assert 'azure-cli' in az_cmd.stdout, "Missing expected 'azure-cli' in version output"
+    assert 'azure-cli' in az_cmd.stdout, \
+        "Missing expected 'azure-cli' in version output"
     assert 'core' in az_cmd.stdout, "Missing expected 'core' in version output"
     assert 'msal' in az_cmd.stdout, "Missing expected 'msal' in version output"
-    assert 'azure-mgmt-resource' in az_cmd.stdout, "Missing expected 'azure-mgmt-resource' in version output"
+    assert 'azure-mgmt-resource' in az_cmd.stdout, \
+        "Missing expected 'azure-mgmt-resource' in version output"
