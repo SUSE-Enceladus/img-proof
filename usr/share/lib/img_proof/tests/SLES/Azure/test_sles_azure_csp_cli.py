9a62172e9538cb6995917262af12b5b4b38a6463
Volodymyr Katkalov <volodymyr.katkalov@suse.com>
2025-08-25 12:13:29 +0300

    Add executable check for CSP CLI
    Co-authored-by: Ricardo Branco <rbranco@suse.de>
    Co-authored-by: Maurizio Dati <maurizio.dati@suse.com>

--- usr/share/lib/img_proof/tests/SLES/Azure/test_sles_azure_csp_cli.py
+++ usr/share/lib/img_proof/tests/SLES/Azure/test_sles_azure_csp_cli.py
@@ -10,8 +10,11 @@ def test_sles_azure_csp_cli(host, is_sle_micro, get_version):
     if version < 16.0:
         pytest.skip('Pre-SLE 16.0 versions do not have CSP CLI.')
 
+    which_cmd = host.run('command -v az')
+    assert which_cmd.rc == 0, f"'{which_cmd.command}' failed with code {which_cmd.rc}."
+
     az_cmd = host.run('az --version')
-    assert az_cmd.rc == 0, f"'az --version' failed with code {az_cmd.rc}"
+    assert az_cmd.rc == 0, f"'{az_cmd.command}' failed with code {az_cmd.rc}"
     assert 'azure-cli' in az_cmd.stdout, \
         "Missing expected 'azure-cli' in version output"
     assert 'core' in az_cmd.stdout, "Missing expected 'core' in version output"
