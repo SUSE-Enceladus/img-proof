e7212faa5989fdc633d7e49d90fa77ed2b4685cf
Sean Marlow <sean.marlow@suse.com>
2025-09-19 14:08:25 -0400

    Bump version: 9.6.0 → 9.7.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'SUSE'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '9.6.0'
+release = '9.7.0'
 
 
 # -- General configuration ---------------------------------------------------
