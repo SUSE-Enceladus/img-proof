4f36394c25a2176703ac38bb8bd892f321dafe04
Andres Pozo <andres.munoz@suse.com>
2025-11-04 20:19:17 +0100

    Fixes SAP MOTD test for version >= 16.0

--- usr/share/lib/img_proof/tests/SLES/SAP/test_sles_sap_motd.py
+++ usr/share/lib/img_proof/tests/SLES/SAP/test_sles_sap_motd.py
@@ -2,7 +2,7 @@ import pytest
 
 
 @pytest.mark.skipinbeta
-def test_sles_sap_motd(host, get_release_value, get_variant):
+def test_sles_sap_motd(host, get_release_value, get_variant, get_version):
     motd = host.file('/etc/motd')
 
     if not motd.exists:
@@ -12,7 +12,13 @@ def test_sles_sap_motd(host, get_release_value, get_variant):
     assert motd.is_file
 
     variant = get_variant()
-    if 'hardened' in variant:
+    version = get_version()
+
+    if (
+        'hardened' in variant or
+        # SAP is hardened by default in >=16.0
+        version >= 16.0
+    ):
         pytest.skip('Unable to validate motd in hardened images.')
 
     assert motd.contains('for SAP Applications')
