80ec3e9f7210a61c8dfbf64bcdb524ef1fb44589
Andres Pozo <andres.munoz@suse.com>
2026-05-12 15:17:42 +0200

    test_sles_motd refactor to handle properly the empty /etc/motd scenario (#454)

--- usr/share/lib/img_proof/tests/SLES/test_sles_motd.py
+++ usr/share/lib/img_proof/tests/SLES/test_sles_motd.py
@@ -9,13 +9,7 @@ def test_sles_motd(
     is_suma_proxy,
     get_variant
 ):
-    motd = host.file('/etc/motd')
 
-    if not motd.exists:
-        motd = host.file('/usr/lib/motd.d/10_header')
-
-    assert motd.exists
-    assert motd.is_file
 
     pretty_name = get_release_value('PRETTY_NAME')
     assert pretty_name
@@ -27,6 +21,14 @@ def test_sles_motd(
 
     if 'hardened' in variant:
         pytest.skip('Unable to validate motd in hardened images.')
+
+    motd = host.file('/etc/motd')
+
+    if not motd.exists or motd.size == 0:
+        motd = host.file('/usr/lib/motd.d/10_header')
+
+    assert motd.exists
+    assert motd.is_file
 
     assert (
         motd.contains(pretty_name) or
@@ -40,31 +42,50 @@ def test_sles_motd(
     # check motd content for all segments
     def contains_registration_tools(motd_file):
         """Checks if any of the registration tools is mentioned in motd"""
-        return (
-            motd_file.contains('registercloudguest') or
-            motd_file.contains('SUSEConnect') or
-            motd_file.contains('suseconnect') or
-            motd_file.contains('transactional-update')
+        tools = [
+            'registercloudguest',
+            'SUSEConnect',
+            'suseconnect',
+            'transactional-update'
+        ]
+        return any(
+            motd_file.contains(tool) for tool in tools
         )
 
+    def registration_tools_found(contain_results):
+        return any(contain_results)
+
     contain_results = []
+    registration_tools_expected = is_byos() and not is_suma_proxy()
 
     motd = host.file('/etc/motd')
     if motd.exists:
         contain_results.append(contains_registration_tools(motd))
-    else:
-        # check for every segment under the motd.d dir
-        motd_dir = '/usr/lib/motd.d'
-        motd_ls_cmd = f'ls {motd_dir}'
-        result = host.run(motd_ls_cmd)
 
-        segments = [filename for filename in result.stdout.strip().split('\n') if filename]  # NOQA
-        for segment in sorted(segments):
-            motd = host.file(f'{motd_dir}/{segment}')
-            if motd.exists and motd.is_file:
-                contain_results.append(contains_registration_tools(motd))
-
-    if is_byos() and not is_suma_proxy():
-        assert contain_results and any(contain_results)
+    if registration_tools_expected:
+        if registration_tools_found(contain_results):
+            # reg tools found and expected, test OK
+            return
     else:
-        assert contain_results and not any(contain_results)
+        if registration_tools_found(contain_results):
+            # reg tools found and not expected, test should fail
+            assert False
+
+    # Check for every segment under the /usr/lib/motd.d directory
+    # if the result of the tests is unknown after checking /etc/motd
+    motd_dir = '/usr/lib/motd.d'
+    motd_ls_cmd = f'ls {motd_dir}'
+    result = host.run(motd_ls_cmd)
+
+    segments = [
+        filename for filename in result.stdout.strip().split('\n') if filename
+    ]
+    for segment in sorted(segments):
+        motd = host.file(f'{motd_dir}/{segment}')
+        if motd.exists and motd.is_file:
+            contain_results.append(contains_registration_tools(motd))
+
+    if registration_tools_expected:
+        assert registration_tools_found(contain_results)
+    else:
+        assert not registration_tools_found(contain_results)
