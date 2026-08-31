4fb2e54498dcaf4792c64f384df51db955481511
Andres Pozo <andres.munoz@suse.com>
2026-08-31 18:12:05 +0200

    Fixes not required import

--- tests/test_ipa_distro.py
+++ tests/test_ipa_distro.py
@@ -21,8 +21,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import time
-
 from img_proof.ipa_distro import Distro
 from img_proof.ipa_exceptions import IpaDistroException
 
